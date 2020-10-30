15bc003a2d6698f12f57f6f3c00ef7d9d0ea15f9
Dominic <46975935+Dom355@users.noreply.github.com>
2020-10-29 19:20:57 -0700

    updated package

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,16 @@
+const express = require("express");
 const app = express();
+
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
 const helmet = require("helmet");
 const compression = require("compression");
-
+const cors = require("cors");
 
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-
-
 
 const port = process.env.PORT || 3000;
 https
