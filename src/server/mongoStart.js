e2b628af82abd48b2d1e9a5e1d658851353c12d2
Dominic <46975935+Dom355@users.noreply.github.com>
2020-10-30 16:12:19 -0700

    Update mongoStart.js

--- src/server/mongoStart.js
+++ src/server/mongoStart.js
@@ -4,8 +4,20 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
   mongoose
-    .connect(config.mongoURI)
+    .connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
 
     .then(() => console.log("connected to Mongodb..."))
     .catch(err => console.log("couldn't connect to MongoDB", err));
+
+  const Cat = mongoose.model('Cat', {
+    name: String
+  });
+
+  const kitty = new Cat({
+    name: 'Zildjian'
+  });
+  kitty.save().then(() => console.log('meow'));
 };
