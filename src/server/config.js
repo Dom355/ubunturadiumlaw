ff6b4ebaa124b610b61fa7157711b7d90d5eaf4f
Dominic <46975935+Dom355@users.noreply.github.com>
2020-10-29 23:30:01 -0700

    Update config.js
    updated mongoURI

--- src/server/config.js
+++ src/server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     urlApi: "http://localhost:3000/laws",
-    mongoURI: "mongodb://admin:m4Mz4LAgmBKQ9tzTjCkzpQzP@ec2-54-185-130-214.us-west-2.compute.amazonaws.com:27017/radiumlaw"
+    mongoURI: "mongodb://admin:m4Mz4LAgmBKQ9tzTjCkzpQzP@localhost:27017/radiumlaw"
 }
