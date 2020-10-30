a29e4f19d17376fe60db22c79fcec8cebff5e52f
Dominic <46975935+Dom355@users.noreply.github.com>
2020-10-29 23:06:29 -0700

    Update config.js
    -updated the mongo connection --> ubuntu is user name

--- src/server/config.js
+++ src/server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     urlApi: "http://localhost:3000/laws",
     mongoEc2: "mongodb: //ec2-user@127.0.0.1:27017/radiumlaw",
-    mongoURI: "mongodb: //ec2-user@127.0.0.1:27017/radiumlaw"
+    mongoURI: "mongodb: //ubuntu@127.0.0.1:27017/radiumlaw"
 }
